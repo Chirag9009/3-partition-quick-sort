61d3231d01f6ca205811934332871fa996e60a2b
Chirag Vijayvargiya <60066552+Chirag9009@users.noreply.github.com>
2020-07-30 22:23:30 +0530

    Update quick3_sort.py

--- quick3_sort.py
+++ quick3_sort.py
@@ -1,10 +1,18 @@
 import random
 
 def partition3(arr, low, high):
+    """
+    fucntion: function that imlements the 3 partition technique
+    input: list of integers, start and end index
+    return value: tuple of 2 integers, 1st int denotes end of 1st partition, 2nd int denotes start of 2nd partition
+    """
     pivot = arr[low]
     i = low+1
-    while i <= high:
-        if arr[i] < pivot:
+    # if value is less than pivot then put it in the 1st partition
+    # if value is greater than pivot then put it in the 2nd partition 
+    # otherwise keep it in the middle
+    while i <= high: 
+        if arr[i] < pivot: 
             arr[i], arr[low] = arr[low], arr[i]
             i += 1
             low += 1
@@ -17,22 +25,35 @@ def partition3(arr, low, high):
 
 
 def q3_sort(arr, low, high):
+    """
+    Function: function that implements the 3 partition quick sort for a list of integers
+    input: an list of integer value
+    return value: a list of sorted integer values
+    """
     while low < high:
-        index = random.randint(low, high)
-        arr[low], arr[index] = arr[index], arr[low]
-        m1, m2 = partition3(arr,low,high)
-        if (m1-low) < (high-m2):
+        index = random.randint(low, high) # choose a random pivot in the array
+        arr[low], arr[index] = arr[index], arr[low] 
+        m1, m2 = partition3(arr,low,high) # m1 = end point of 1st partition and m2 = start of 2nd partition
+        if (m1-low) < (high-m2): # if 1st partition is smaller then recursively sort it first, solve 2nd partition otherwise
             q3_sort(arr, low, m1-1)
             low = m2+1
-        else:
+        else: 
             q3_sort(arr, m2+1, high)
             high = m1-1
     return arr
 
-
-n = int(input())
-arr = list(map(int, input().split()))
-arr = q3_sort(arr, 0, len(arr)-1)
-for i in arr:
-    print(i, end=" ")
-print()
+def main():
+    """
+    function: Driver code that handles input and calls the sorting procedure
+    input: None
+    return: None
+    """
+    arr = list(map(int, input().split()))
+    arr = q3_sort(arr, 0, len(arr)-1)
+    for i in arr:
+        print(i, end=" ")
+    print()
+    
+    
+if __name__ == "__main__":
+    main()
